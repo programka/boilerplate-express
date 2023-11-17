36fb716f1cb68c6754f43fa1dd3f4eab81df0139
programka <70807719+programka@users.noreply.github.com>
2023-11-17 13:43:56 +0000

    complete 4-7 tasks

--- myApp.js
+++ myApp.js
@@ -1,5 +1,27 @@
 let express = require('express');
 let app = express();
+console.log("Hello World");
+
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+app.get('/', (_, res) => {
+  absolutePath = __dirname + "/views/index.html"
+  res.sendFile(absolutePath);
+ // res.send('Hello Express');
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/json', (_, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase"){
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+})
 
 
 
@@ -34,6 +56,4 @@ let app = express();
 
 
 
-
-
- module.exports = app;
+module.exports = app;
