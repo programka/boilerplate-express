f6829a031df145b8cf13ffbec379746d1d86337a
programka <70807719+programka@users.noreply.github.com>
2023-11-18 21:11:48 +0000

    complete 8-12 tasks: GET, POST methods

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+let bodyParser = require('body-parser');
+
 let express = require('express');
 let app = express();
 console.log("Hello World");
@@ -10,22 +12,50 @@ app.use((req, res, next) => {
 app.get('/', (_, res) => {
   absolutePath = __dirname + "/views/index.html"
   res.sendFile(absolutePath);
- // res.send('Hello Express');
+  // res.send('Hello Express');
 })
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/json', (_, res) => {
-  if (process.env.MESSAGE_STYLE == "uppercase"){
-    res.json({"message": "HELLO JSON"});
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ "message": "HELLO JSON" });
   } else {
-    res.json({"message": "Hello json"});
+    res.json({ "message": "Hello json" });
   }
 })
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.send({ time: req.time });
+});
 
+app.get('/:word/echo', function(req, res, next) {
+  word = req.params.word;
+  next();
+}, function(req, res) {
+  res.send({ echo: word });
+});
 
+app.get('/name', function(req, res, next) {
+  firstname = req.query.first;
+  lastname = req.query.last;
+  next();
+}, function(req, res) {
+  res.send({ name: firstname + ' ' + lastname });
+});
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/name', function(req, res, next) {
+  firstname = req.body.first;
+  lastname = req.body.last;
+  next();
+}, function(req, res) {
+  res.send({ name: firstname + ' ' + lastname });
+});
 
 
 
